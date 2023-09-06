d20e8b2461980c992533645dbf2831c30c866c2d
Ashutosh Mehra <asmehra@redhat.com>
2023-09-06 12:30:44 -0400

    Separate out rampup measurement from run_tput.sh
    Signed-off-by: Ashutosh Mehra <asmehra@redhat.com>

--- run_warmup.sh
+++ run_warmup.sh
@@ -9,10 +9,6 @@ prereqs() {
     echo "env var APP_JAR must be set"
     exit 1
   fi
-  if [ -z "${JMETER_HOME}" ]; then
-    echo "env var JMETER_HOME must be set"
-    exit 1
-  fi
 }
 
 prereqs
@@ -20,48 +16,42 @@ prereqs
 source ./utils.sh
 
 #===============================
-# Baseline run with a warmup phase and 10 jmeter threads
+# Baseline run with 1 jmeter thread
 #===============================
 test_prereqs
 echo
-log "Baseline run with a warmup phase and 10 jmeter threads"
+log "Baseline run with 1 jmeter thread"
 CONFIG="baseline"
-RESULTS_DIR="${APP_NAME}/${CONFIG}/tput-t10"
+RESULTS_DIR="${APP_NAME}/${CONFIG}/tput-t1"
 cleanup_dir ${RESULTS_DIR}
 create_dir ${RESULTS_DIR}
 APP_LOG_FILE="${RESULTS_DIR}/${APP_NAME}.log"
 JVM_OPTIONS="-Xlog:cds -XX:SharedArchiveFile=${BASELINE_DYNAMIC_CDS_NAME}"
 start_app
-JMETER_WARMUP_LOG_FILE="${RESULTS_DIR}/jmeter.warmup.log"
-warmup
-JMETER_THREADS=10
+JMETER_THREADS=1
 LOAD_DURATION=300
 JMETER_LOG_FILE="${RESULTS_DIR}/jmeter.log"
 generate_load
 stop_app
-get_tput_results "${JMETER_WARMUP_LOG_FILE}" "${RESULTS_DIR}/warmup"
 get_tput_results "${JMETER_LOG_FILE}" "${RESULTS_DIR}/tput"
 
 #===============================
-# AOT run with a warmup phase and 10 jmeter threads
+# AOT run with 1 jmeter thread
 #===============================
 test_prereqs
 echo
-log "AOT run with a warmup phase and 10 jmeter threads"
+log "AOT run with 1 jmeter thread"
 CONFIG="aot"
-RESULTS_DIR="${APP_NAME}/${CONFIG}/tput-t10"
+RESULTS_DIR="${APP_NAME}/${CONFIG}/tput-t1"
 cleanup_dir ${RESULTS_DIR}
 create_dir ${RESULTS_DIR}
 APP_LOG_FILE="${RESULTS_DIR}/${APP_NAME}.log"
 JVM_OPTIONS="-Xlog:cds -XX:SharedArchiveFile=${TRAINING_DYNAMIC_CDS_NAME} -XX:+ReplayTraining -XX:+LoadCachedCode -XX:CachedCodeFile=${SCA_NAME}"
 start_app
-JMETER_WARMUP_LOG_FILE="${RESULTS_DIR}/jmeter.warmup.log"
-warmup
-JMETER_THREADS=10
+JMETER_THREADS=1
 LOAD_DURATION=300
 JMETER_LOG_FILE="${RESULTS_DIR}/jmeter.log"
 generate_load
 stop_app
-get_tput_results "${JMETER_WARMUP_LOG_FILE}" "${RESULTS_DIR}/warmup"
 get_tput_results "${JMETER_LOG_FILE}" "${RESULTS_DIR}/tput"
 
